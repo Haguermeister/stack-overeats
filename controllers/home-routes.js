e5643f9aff5a0de62be748c718576c12e5a70ad4
jreese8 <johnathanreese8@gmail.com>
2022-03-09 21:48:13 -0500

    Removed ALL my changes to home-routes.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,10 +2,6 @@ const router = require('express').Router();
 // Import the custom middleware
 const withAuth = require('../utils/auth');
 
-router.get('/', (req, res) => {
-  res.render('homepage')
-});
-
 router.get('/login', (req, res) => {
   if (req.session.loggedIn) {
     res.redirect('/');
